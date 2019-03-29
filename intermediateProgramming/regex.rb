0f3afafee0e8384cfde97bc075f103ae802368d1
Kevin Bai <lkevin957@gmail.com>
2019-03-29 11:45:57 -0700

    complete regex

---
+++ intermediateProgramming/regex.rb
@@ -0,0 +1,17 @@
+def my_expression()
+# Write your code here.Return a string version of a regular expression.
+    "/.+/+"
+end
+
+
+
+
+# Input https://www.talentmatch.ai/welcome
+
+# Output //www.talentmatch.ai/
+
+
+# def my_expression()
+# # Write your code here.Return a string version of a regular expression.
+#     "^.+w+"
+# end
